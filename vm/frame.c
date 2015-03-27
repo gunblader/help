538fd646fbae2889ebdcd35482b38c0749ccc2d2
Kenneth Hollinger <krh2464@cs.utexas.edu>
2015-03-27 12:31:04 -0500

    Updated questions.txt

---
+++ vm/frame.c
@@ -0,0 +1,8 @@
+
+#include "lib/kernel/list.h"
+
+
+/* Global Frame table list that contains all the available frames that pages can be mapped to*/
+struct list frame_table;
+
+
---
+++ vm/frame.h
@@ -0,0 +1,6 @@
+
+
+struct frame
+{
+	struct page *cur_page; /* A pointer to the page that is currently using this frame */
+};
